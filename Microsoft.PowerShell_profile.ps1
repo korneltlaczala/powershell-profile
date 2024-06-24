1f87344b309c8b862e6d9094bdd0fc783e400b75
Kornel <kornel.tlaczala@gmail.com>
2024-06-25 00:53:53 +0200

    quick test 3

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,7 +16,7 @@
 ############                                                                                                         ############
 #################################################################################################################################
 
-# test Update-Profile function 2
+# test Update-Profile function 3
 
 # Color variables
 $fetchColor = "Cyan"
