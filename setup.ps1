b85b8545ad5b1ee1ca3c81b5b5cdcfcbd757d74e
Kornel <kornel.tlaczala@gmail.com>
2024-06-24 12:25:21 +0200

    change iex to full command for readibility

--- setup.ps1
+++ setup.ps1
@@ -70,7 +70,7 @@ catch {
 # Choco install
 try {
     Write-Host "Installing Chocolatey..." -ForegroundColor $informationColor
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     Write-Host "Chocolatey ready." -ForegroundColor $successColor
 }
 catch {
