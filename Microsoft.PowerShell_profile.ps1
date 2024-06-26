9fef385b55eb3046d4c67d16d0cce93317f98334
Kornel <kornel.tlaczala@gmail.com>
2024-06-26 03:35:47 +0200

    unlock functions part 2

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -212,100 +212,100 @@ function reload-profile {
     . $profile
 }
 
-# function unzip ($file) {
-#     Write-Output("Extracting", $file, "to", $pwd)
-#     $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
-#     Expand-Archive -Path $fullFile -DestinationPath $pwd
-# }
-# function hb {
-#     if ($args.Length -eq 0) {
-#         Write-Error "No file path specified."
-#         return
-#     }
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+function hb {
+    if ($args.Length -eq 0) {
+        Write-Error "No file path specified."
+        return
+    }
     
-#     $FilePath = $args[0]
+    $FilePath = $args[0]
     
-#     if (Test-Path $FilePath) {
-#         $Content = Get-Content $FilePath -Raw
-#     } else {
-#         Write-Error "File path does not exist."
-#         return
-#     }
+    if (Test-Path $FilePath) {
+        $Content = Get-Content $FilePath -Raw
+    } else {
+        Write-Error "File path does not exist."
+        return
+    }
     
-#     $uri = "http://bin.christitus.com/documents"
-#     try {
-#         $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
-#         $hasteKey = $response.key
-#         $url = "http://bin.christitus.com/$hasteKey"
-#         Write-Output $url
-#     } catch {
-#         Write-Error "Failed to upload the document. Error: $_"
-#     }
-# }
-# function grep($regex, $dir) {
-#     if ( $dir ) {
-#         Get-ChildItem $dir | select-string $regex
-#         return
-#     }
-#     $input | select-string $regex
-# }
+    $uri = "http://bin.christitus.com/documents"
+    try {
+        $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
+        $hasteKey = $response.key
+        $url = "http://bin.christitus.com/$hasteKey"
+        Write-Output $url
+    } catch {
+        Write-Error "Failed to upload the document. Error: $_"
+    }
+}
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
 
-# function df {
-#     get-volume
-# }
+function df {
+    get-volume
+}
 
-# function sed($file, $find, $replace) {
-#     (Get-Content $file).replace("$find", $replace) | Set-Content $file
-# }
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
 
-# function which($name) {
-#     Get-Command $name | Select-Object -ExpandProperty Definition
-# }
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
 
-# function export($name, $value) {
-#     set-item -force -path "env:$name" -value $value;
-# }
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
 
-# function pkill($name) {
-#     Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
-# }
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
 
-# function pgrep($name) {
-#     Get-Process $name
-# }
+function pgrep($name) {
+    Get-Process $name
+}
 
-# function head {
-#   param($Path, $n = 10)
-#   Get-Content $Path -Head $n
-# }
+function head {
+  param($Path, $n = 10)
+  Get-Content $Path -Head $n
+}
 
-# function tail {
-#   param($Path, $n = 10, [switch]$f = $false)
-#   Get-Content $Path -Tail $n -Wait:$f
-# }
+function tail {
+  param($Path, $n = 10, [switch]$f = $false)
+  Get-Content $Path -Tail $n -Wait:$f
+}
 
-# # Quick File Creation
-# function nf { param($name) New-Item -ItemType "file" -Path . -Name $name }
+# Quick File Creation
+function nf { param($name) New-Item -ItemType "file" -Path . -Name $name }
 
-# # Directory Management
-# function mkcd { param($dir) mkdir $dir -Force; Set-Location $dir }
+# Directory Management
+function mkcd { param($dir) mkdir $dir -Force; Set-Location $dir }
 
-# ### Quality of Life Aliases
+### Quality of Life Aliases
 
-# # Navigation Shortcuts
-# function docs { Set-Location -Path $HOME\Documents }
+#no-clear Navigation Shortcuts
+function docs { Set-Location -Path $HOME\Documents }
 
-# function dtop { Set-Location -Path $HOME\Desktop }
+function dtop { Set-Location -Path $HOME\Desktop }
 
-# # Quick Access to Editing the Profile
-# function ep { vim $PROFILE }
+# Quick Access to Editing the Profile
+function ep { vim $PROFILE }
 
-# # Simplified Process Management
-# function k9 { Stop-Process -Name $args[0] }
+# Simplified Process Management
+function k9 { Stop-Process -Name $args[0] }
 
-# # Enhanced Listing
-# function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
-# function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
+# Enhanced Listing
+function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
+function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
 
 # # Git Shortcuts
 # function gs { git status }
