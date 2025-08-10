d25f9caa1560ef2bfa8c2215f9f7544cc91dd900
Kornel <kornel.tlaczala@gmail.com>
2025-08-10 14:46:10 +0200

    testing

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -523,12 +523,12 @@ if (Test-Path "$profilePath\lastProfileUpdate.log") {
         $profile_update_skipped = $true
     }
 }
-if (-not $powershell_update_skipped) {
-    Update-PowerShell
-}
-if (-not $profile_update_skipped) {
-    Update-Profile
-}
+# if (-not $powershell_update_skipped) {
+#     Update-PowerShell
+# }
+# if (-not $profile_update_skipped) {
+#     Update-Profile
+# }
 Write-Host "----------------------------------------"
 Write-Host "Use 'Show-Help' to display help"
 Write-Host "----------------------------------------"
